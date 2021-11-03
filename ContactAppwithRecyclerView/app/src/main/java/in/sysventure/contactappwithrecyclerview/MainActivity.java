5db7cd3a96536fcba2fe7c9b1ba85e6dd1f4be9e
Nitesh Nidarshan <niteshfromkatihar@gmail.com>
2021-11-03 20:16:48 +0530

    RecyclerView modified

--- ContactAppwithRecyclerView/app/src/main/java/in/sysventure/contactappwithrecyclerview/MainActivity.java
+++ ContactAppwithRecyclerView/app/src/main/java/in/sysventure/contactappwithrecyclerview/MainActivity.java
@@ -22,7 +22,17 @@ public class MainActivity extends AppCompatActivity {
             new Contact(6, "1234567895", "Krishna G"),
             new Contact(7, "1234567896", "Radha G"),
             new Contact(8, "1234567897", "Rukkmini G"),
-            new Contact(9, "1234567898", "Dau G")
+            new Contact(9, "1234567898", "Dau G"),
+            new Contact(10, "12345678901", "1 Ram G"),
+            new Contact(11, "12345678919", "2 Sita G"),
+            new Contact(12, "12345678929", "3 Laxman G"),
+            new Contact(13, "12345678939", "4 Bharat G"),
+            new Contact(14, "12345678949", "5 Shatrughan G"),
+            new Contact(15, "12345678959", "6 Krishna G"),
+            new Contact(16, "12345678969", "7 Radha G"),
+            new Contact(17, "12345678979", "8 Rukkmini G"),
+            new Contact(18, "12345678989", "9 Dau G")
+
     };*/
 
     Contact c1 = new Contact(1, "1234567890", "Ram G");
@@ -34,8 +44,17 @@ public class MainActivity extends AppCompatActivity {
     Contact c7 = new Contact(7, "1234567896", "Radha G");
     Contact c8 = new Contact(8, "1234567897", "Rukkmini G");
     Contact c9 = new Contact(9, "1234567898", "Dau G");
+    Contact c10 = new Contact(10, "12345678901", "a Ram G9");
+    Contact c11 = new Contact(11, "12345678911", "a Sita G9");
+    Contact c12 = new Contact(12, "12345678921", "a Laxman G9");
+    Contact c13 = new Contact(13, "12345678931", "a Bharat G9");
+    Contact c14 = new Contact(14, "12345678941", "a Shatrughan G9");
+    Contact c15 = new Contact(15, "12345678951", "a Krishna G9");
+    Contact c16 = new Contact(16, "12345678961", "a Radha G9");
+    Contact c17 = new Contact(17, "12345678971", "a Rukkmini G9");
+    Contact c18 = new Contact(18, "12345678981", "a Dau G9");
 
-    Contact[] contacts = {c1,c2,c3,c4,c5,c6,c7,c8,c9};
+    Contact[] contacts = {c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12,c13,c14,c15,c16,c17,c18};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- ContactAppwithRecyclerView/app/src/main/java/in/sysventure/contactappwithrecyclerview/model/Contact.java
+++ ContactAppwithRecyclerView/app/src/main/java/in/sysventure/contactappwithrecyclerview/model/Contact.java
@@ -1,6 +1,7 @@
 package in.sysventure.contactappwithrecyclerview.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class Contact implements Serializable {
 
@@ -24,6 +25,19 @@ public class Contact implements Serializable {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return serialNumber == contact.serialNumber && phoneNumber.equals(contact.phoneNumber) && name.equals(contact.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(serialNumber, phoneNumber, name);
     }
 
     @Override
